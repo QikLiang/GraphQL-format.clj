0cfee0dd2cdcb282fbab4ae6eb78e219eab6b05c
qikliang <qik.liang@gmail.com>
2020-05-11 00:16:41 -0700

    create macro to convert binding parameters from special symbols to namespace-qualified keywords

--- src/gql_format/core.clj
+++ src/gql_format/core.clj
@@ -1,6 +1,22 @@
-(ns gql-format.core)
+(ns gql-format.core
+  (:require [clojure.walk :refer [postwalk]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; prefix namespace used to ensure keywords are unique
+(def prefix "gqlf/")
+
+(defmacro qualify
+  "recursively find all symbols of the form ?param and convert
+   them into the form :gqlf/param"
+  [query]
+  (letfn [(convert [sym]
+            (as-> sym $
+              (name $)
+              (subs $ 1)
+              (str prefix $)
+              (keyword $)))
+          (check-convert [obj]
+            (if (and (symbol? obj)
+                     (= \? (first (name obj))))
+              (convert obj)
+              obj))]
+    (postwalk check-convert query)))
--- test/gql_format/core_test.clj
+++ test/gql_format/core_test.clj
@@ -1,7 +1,21 @@
 (ns gql-format.core-test
   (:require [clojure.test :refer :all]
-            [gql-format.core :refer :all]))
+            [gql-format.core :as gf]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest simple-qualifier
+  (testing "qualify a simgle symbol"
+    (is (= :gqlf/symbol (gf/qualify ?symbol)))))
+
+(deftest flat-map-qualifier
+  (testing "qualify a flat map"
+    (is (= {:key :gqlf/value} (gf/qualify {:key ?value})))))
+
+(deftest nested-map-qualifier
+  (testing "qualify a nested map"
+    (is (= {:k2 {:key :gqlf/value} :unrelated :value}
+           (gf/qualify {:k2 {:key ?value} :unrelated :value})))))
+
+(deftest multiple-qualifiers
+  (testing "qualify multiple symbols in a map"
+    (is (= {:k1 :gqlf/v1 :k2 :gqlf/v2}
+           (gf/qualify {:k1 ?v1 :k2 ?v2})))))
