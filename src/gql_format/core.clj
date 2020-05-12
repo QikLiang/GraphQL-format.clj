0b1dfc6501d745bd1f27b7238d30124746d5dc60
qikliang <qik.liang@gmail.com>
2020-05-12 00:39:34 -0700

    convert output to query

--- src/gql_format/core.clj
+++ src/gql_format/core.clj
@@ -1,5 +1,6 @@
 (ns gql-format.core
-  (:require [clojure.walk :refer [postwalk]]))
+  (:require [clojure.walk :refer [postwalk]]
+            [clojure.string :as str]))
 
 ; prefix namespace used to ensure keywords are unique
 (def prefix "gqlf/")
@@ -20,3 +21,68 @@
               (convert obj)
               obj))]
     (postwalk check-convert query)))
+
+(defn qualified?
+  "check if value matches the format of the output of qualify"
+  [value]
+  (and (keyword? value) (= "gqlf" (namespace value))))
+
+(defn build
+  "build a GraphQL query string using the expected output shape"
+  [output]
+  (letfn [(convert-fields [fields]
+            ; take a mapping of fields to converted text and
+            ; join them to form a block
+            (str "{"
+                 (str/join " " (for [[k v] fields]
+                                 (str k (first v) (second v))))
+                 "}"))
+          (convert-params [ps]
+            ; take a list of key value pairs of GraphQL
+            ; field arguments and join them
+            (if (empty? ps) ""
+              (str "("
+                   (str/join ", "
+                             (for [[k v] ps
+                                   :when (not (qualified? k))]
+                               (str (name k) ": " v)))
+                   ")")))
+          (convert-map [m]
+            ; separate a map into arguments and fields,
+            ; convert them separately, and return a tuple
+            (let [keywords (group-by #(keyword? (first %))
+                                     (dissoc m :gqlf/as))]
+              (list
+                (convert-params (keywords true))
+                (convert-fields (keywords false)))))
+          (convert-vec [v]
+            ; treat the first 2N elements such that the even
+            ; indices are keywords as argument key value pairs,
+            ; return a tuple of the converted arguments and
+            ; the 2N+1th element. Assume 2N+1th element is
+            ; output from convert with no arguments
+            (loop [vnext v
+                   args []]
+              (cond
+                (= (first vnext) :gqlf/as)
+                (recur (drop 2 vnext) args)
+
+                (keyword? (first vnext))
+                (recur (drop 2 vnext) (conj args (take 2 vnext)))
+
+                :else
+                (list (convert-params args) (second (first vnext))))))
+          ; treat map entries differently than normal vectors
+          (map-entry? [obj]
+            (and (vector? obj) (= 2 (count obj))))
+          ; dispatch to the proper convertor based on data shape
+          (convert [obj]
+            (cond
+              (map? obj) (convert-map obj)
+              (= obj :gqlf/as) obj
+              (qualified? obj) (list "" "")
+              (map-entry? obj) obj
+              (vector? obj) (convert-vec obj)
+              :else obj))]
+    ; only return the fields part of the tuple
+    (second (postwalk convert output))))
--- test/gql_format/core_test.clj
+++ test/gql_format/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [gql-format.core :as gf]))
 
+; qualify tests
 (deftest simple-qualifier
   (testing "qualify a simgle symbol"
     (is (= :gqlf/symbol (gf/qualify ?symbol)))))
@@ -19,3 +20,47 @@
   (testing "qualify multiple symbols in a map"
     (is (= {:k1 :gqlf/v1 :k2 :gqlf/v2}
            (gf/qualify {:k1 ?v1 :k2 ?v2})))))
+
+
+; build tests
+(deftest build-flat-query
+  (testing "build a query from the simplest output shape"
+    (is (= (gf/build (gf/qualify {"field1" ?param1 "field2" ?_}))
+           "{field1 field2}"))))
+
+(deftest build-nested-query
+  (testing "build a query with nested fields"
+    (is (= (gf/build (gf/qualify
+                       {"field1" ?param1
+                        "field2" ?_
+                        "field3" {"nested1" ?n1
+                                  "nested2" ?n2}}))
+           "{field1 field2 field3{nested1 nested2}}"))))
+
+(deftest build-argument-query
+  (testing "build a query with argument fields"
+    (is (= (gf/build (gf/qualify
+                       {"field1" ?param1
+                        "field2" {:arg1 "val1"
+                                  :arg2 3
+                                  "nested1" ?n1
+                                  "nested2" ?n2}}))
+           "{field1 field2(arg1: val1, arg2: 3){nested1 nested2}}"))))
+
+(deftest build-list-query
+  (testing "build a query containing a list field"
+    (is (= (gf/build (gf/qualify
+                       {"field1" ?param1
+                        "field2" [:arg1 3
+                                  :arg2 "hi"
+                                  {"field3" ?_}]}))
+           "{field1 field2(arg1: 3, arg2: hi){field3}}"))))
+
+(deftest build-as-query
+  (testing "build a query with ?as bindings"
+    (is (= (gf/build (gf/qualify
+                       {?as ?all
+                        "field1" [?as ?list
+                                  {?as ?list-item
+                                   "field2" ?val}]}))
+           "{field1{field2}}"))))
